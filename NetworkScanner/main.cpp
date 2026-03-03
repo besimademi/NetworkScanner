5d8b99748ad82f0fb0d8fadb598b7013a27e103c
besimademi <besim.ademi2004@proton.me>
2026-03-03 21:43:05 +0100

    I did with "threads.emplace_back(ping, ip);" cuz the async ..... was so slow

--- NetworkScanner/main.cpp
+++ NetworkScanner/main.cpp
@@ -2,6 +2,7 @@
 #include <string>
 #include <thread>
 #include <future>
+#include <vector>
 #include <cstdlib>
 using namespace std;
 
@@ -11,10 +12,16 @@ void ping(string ip)
 }
 int main()
 {
+	vector<thread> threads;
 	for (int i = 1; i < 255; i++)
 	{
 		string ip = "ping 192.168.0." + to_string(i);
-		async(launch::async, ping, ip);
-
+		//async(launch::async, ping, ip);
+		threads.emplace_back(ping, ip);
 	}
+	for (auto& i : threads)
+	{
+		i.join();
+	}
+
 }
