0b1b3e3a8528d3589f81c18db4af47572643cb68
besimademi <besim.ademi2004@proton.me>
2026-03-03 23:50:20 +0100

    I fixed the bug before.

--- NetworkScanner/main.cpp
+++ NetworkScanner/main.cpp
@@ -3,25 +3,52 @@
 #include <thread>
 #include <future>
 #include <vector>
+#include <mutex>
 #include <cstdlib>
 using namespace std;
-
-void ping(string ip)
+mutex mtx;
+void ping(string ip,vector<string>& valid_ip)
 {
-	system(ip.c_str());
+	/*there was problem because before always it turn result 0 even if the host is not in the network
+	so i combined 2 commands to see if it works it does*/
+	string cmd = "ping " + ip + " | findstr \"TTL\" ";
+
+	int result=system(cmd.c_str());
+	if (result == 0)
+	{
+		//idk what mutex is chat suggested me this ( it is used to prevent multiple threads from accessing the same resource at the same time ) but i dont understnda in details bow it works
+// it works
+		lock_guard<mutex> lock(mtx);
+		valid_ip.push_back(ip);
+		cout << ip << " is reachable" << endl;
+	}
+
 }
 int main()
 {
+	// vecotr for threads and valid ips
+	vector<string> valid_ip;
 	vector<thread> threads;
 	for (int i = 1; i < 255; i++)
 	{
-		string ip = "ping 192.168.0." + to_string(i);
+		// generate all the ips
+		string ip = "192.168.0." + to_string(i);
 		//async(launch::async, ping, ip);
-		threads.emplace_back(ping, ip);
+		// i commented the async cuz it was to slow
+		// i am making to much threads but we will se to fix this later
+		threads.emplace_back(ping, ip,ref(valid_ip));
 	}
+
+	// wait for all threads to finish
 	for (auto& i : threads)
 	{
 		i.join();
 	}
 
+	// we show here all the valid ips that we found in the network
+	cout << endl << endl << endl << endl;
+	for (auto& t : valid_ip)
+	{
+		cout <<"Ip :" << t <<" is reachable" << endl;
+	}
 }
